88f27157cc4328675db9af206356fe88a67ef452
Craig Jones <jones.dcraig@gmail.com>
2018-09-23 23:46:40 -0400

    Added reactive response: hovering over the cross-section (right or bottom axis) will plot the corresponding point on the main panel.  Fixed bug that caused value to persist after cross-section have changed (due to a new click on the main panel).

--- app.R
+++ app.R
@@ -4,26 +4,27 @@ source("C:/Users/Craig/Dropbox/_UNF/Craig Thesis/R-Roughness/R/Roughness/L1.R")
 
 
 # GUI Interface -----------------------------------------------------------
+# See Notes at EOF for Variable Naming Scheme
 
 ui <- basicPage(
     fluidRow(
       column(width=6,
         plotOutput("main"
           , height = "350px"
-          , click = "mce"
-          , dblclick = "mde"
-          , hover = "mhe"
-          , brush = "mbe"
+          , click = "mcd"
+          , dblclick = "mdd"
+          , hover = "mhd"
+          , brush = "mbd"
         )#plotOutput
       )#column
       
       , column(width=3,
           plotOutput("right"
             , height="350px"
-            , click="rce"
-            , dblclick = "rde"
-            , hover = "rhe"
-            , brush="rbe"
+            , click="rcd"
+            , dblclick = "rdd"
+            , hover = "rhd"
+            , brush="rbd"
           )#plotOutput
         )#column
       )#fluidRow
@@ -32,16 +33,14 @@ ui <- basicPage(
         column(width=6,
           plotOutput("bottom"
             , height="250px"
-            , click="bce"
-            , dblclick="bde"
-            , hover="bhe"
-            , brush=brushOpts(id="bbe", direction="x")
+            , click="bcd"
+            , dblclick="bdd"
+            , hover="bhd"
+            , brush=brushOpts(id="bbd", direction="x")
           )#plotOutput
         )#column
-    )#fluidRow
-    
-    , fluidRow(
-        column(width=9,
+
+        , column(width=3,
                verbatimTextOutput("info")
         )#column
     )#fluidRow
@@ -54,25 +53,40 @@ ui <- basicPage(
 server <- function(input, output) {
     w <- dim(z)[1]
     h <- dim(z)[2]
-  
+    
     mca <- reactiveValues(x = 100, y= 100)
     mcr <- reactiveValues(x = 0.5, y=0.5)
     
-    observeEvent(input$mce$x, {
-        mcr$x <- input$mce$x
+    rdf <- reactive({data.frame(x=z[mca$x,], y=1:h)})
+    rha <- reactive({nearPoints(rdf(), input$rhd, xvar="x", yvar="y", threshold=10,maxpoints=1)})
+    
+    bdf <- reactive({data.frame(y=z[,mca$y], x=1:w)})
+    bha <- reactive({nearPoints(bdf(), input$bhd, xvar="x", yvar="y", threshold=10,maxpoints=1)})
+    
+    observeEvent(input$mcd$x, {
+        mcr$x <- input$mcd$x
         mca$x <- round(mcr$x*w,0)
     })
     
-    observeEvent(input$mce$y, {
-        mcr$y <- input$mce$y
+    observeEvent(input$mcd$y, {
+        mcr$y <- input$mcd$y
         mca$y <- round(mcr$y*h,0)
     })
-  
 
   output$main <- renderPlot({
+    # a <- round(rha()[1],2)
+    # b <- round(bha()[1],2)
+    # depth <- max(a,b)
+    
     image(z, axes=F, useRaster=T)
     abline(v=mcr$x, h=mcr$y)
     box(lwd=3)
+    
+    points(mcr$x, rha()[2]/h, pch=3)
+    #text(mcr$x, rha()[2]/h, depth, pos=2)
+    
+    points(bha()[2]/w, mcr$y, pch=3)
+    #text(bha()[2]/w, mcr$y, depth, pos=1)
   })#main
   
   output$bottom <- renderPlot({
@@ -83,14 +97,11 @@ server <- function(input, output) {
   output$right <- renderPlot({
     plot(z[mca$x,],1:dim(z)[2], type="l", lwd=2)
     abline(h=mca$y)
-    
   })
    
   output$info <- renderText({
-    paste0(  "x: ", input$mcr$x, "\n"
-           , "y: ", input$mcr$y, "\n"
-           , "x2:", mca$x, "\n"
-           , "y2:", mca$y, "\n"
+   
+    paste0(rha()
            )
   })#renderText
 }#server
@@ -99,3 +110,22 @@ server <- function(input, output) {
 
 shinyApp(ui = ui, server = server)
 
+
+# Notes -------------------------------------------------------------------
+
+### Naming Scheme
+#
+#       Event variable names are defined as a three-letter word.
+#         First letter defines the panel:
+#             • m = main
+#             • r = right (y-axis)
+#             • b = bottom (x-axis)
+#         Second letter defines the event:
+#             • c = click
+#             • d = double-click
+#             • h = hover
+#             • b = brush (aka window select)
+#         Third value defines the coordinate system
+#             • a = absolute (values from 0 to ...)
+#             • r = relative (values from 0% to 100%)
+#             • d = direct (direct, unaltered input from the event)
